3470670e1761c4493136ac715f4fa25eda63deaa
Oleh Karmazin <kalemzwork@gmail.com>
2025-07-30 10:24:57 +0200

    Initial commit

---
+++ src/main/java/com/kafka/atiperaproject/AtiperaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.atiperaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtiperaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafka/atiperaproject/AtiperaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.atiperaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiperaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
